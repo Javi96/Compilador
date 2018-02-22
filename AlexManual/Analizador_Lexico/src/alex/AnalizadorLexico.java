2338ff2f7098dac533bcfb12b7214e4d65cd1e4a
Elena Kaloyanova Popova <elena.dokrai@gmail.com>
2018-02-22 11:44:22 +0100

    Fase1 - ManualV1.1
    Implementación completa.

--- AlexManual/Analizador_Lexico/src/alex/AnalizadorLexico.java
+++ AlexManual/Analizador_Lexico/src/alex/AnalizadorLexico.java
@@ -25,7 +25,7 @@ public class AnalizadorLexico {
     INICIO, R_PAP, R_PCI, R_ASIG, R_IGUAL, R_MEN,
     R_MENI, R_MAY, R_MAYI, R_EXL,R_DIST,R_VAR,
     R_FIN,R_AMP,R_SEC,R_POR,R_DIV,R_MAS,R_MENOS,
-    R_ENT,R_P,R_DEC,R_E,R_DIG,R_EX,R_AND,R_NOT,R_OR
+    R_ENT,R_P,R_DEC,R_E,R_SIGNO,R_EX
    }
 
    private Estado estado;
@@ -60,7 +60,7 @@ public class AnalizadorLexico {
 			//SI PAP -> TRANSITA R_PAP
 			else if(hayPAp()) transita(Estado.R_PAP);
 			//SI ASIG -> TRANSITA R_ASIG
-			else if(hayAsignacion()) transita(Estado.R_ASIG);
+			else if(hayIgual()) transita(Estado.R_ASIG);
 			//SI MEN -> TRANSITA R_MEN
 			else if(hayMenor()) transita(Estado.R_MEN);
 			//SI MAY -> TRANSITA R_MAY
@@ -92,100 +92,83 @@ public class AnalizadorLexico {
 			if(hayAmp()) transita(Estado.R_SEC);
 			else error();
 			break;
-		case R_AND:
-			//no se muy bien que hacer aqui
-			break;
 		case R_ASIG:
-			if(hayAsignacion()) transita(Estado.R_IGUAL);
-			return unidadAsig();
+			if(hayIgual()) transita(Estado.R_IGUAL);
+			else return unidadAsig();
 		case R_DEC:
 			if(hayDigito()) transita(Estado.R_DEC);
 			else if(hayExponente()) transita(Estado.R_EX);
-			else error();
-			break;
-		case R_DIG:
+			else return unidadNumR();
+		case R_SIGNO:
 			if(hayDigito()) transita(Estado.R_EX);
 			else error();
 			break;
-		case R_DIST:
-			return unidadId();
-		case R_DIV:
-			return unidadDiv();
+		case R_DIST: return unidadDist();
+		case R_DIV: return unidadDiv();
 		case R_E:
-			if(haySuma() || hayResta()) transita(Estado.R_DIG);
+			if(haySuma() || hayResta()) transita(Estado.R_SIGNO);
 			else error();
 			break;
 		case R_ENT:
 			if(hayDigito()) transita(Estado.R_ENT);
 			else if(hayPunto()) transita(Estado.R_P);
 			else if(hayExponente()) transita(Estado.R_E);
-			else error();
+			else return unidadNumR();
 			break;
-		case R_EX:
-			return unidadEx();
+		case R_EX: return unidadNumR();
 		case R_EXL:
-			if(hayAsignacion()) transita(Estado.R_DIST);
+			if(hayIgual()) transita(Estado.R_DIST);
 			else error();
 			break;
-		case R_FIN:
-			return unidadId();
-		case R_IGUAL:
-			return unidadId();
+		case R_FIN: return unidadEOF();
+		case R_IGUAL: return unidadIgual();
 		case R_MAS:
 			if(hayDigito()) transita(Estado.R_ENT);
-			return unidadMas();
+			else return unidadMas();
 		case R_MAY:
-			if(hayAsignacion()) transita(Estado.R_MAYI);
-			return unidadMay();
-		case R_MAYI:
-			return unidadId();
+			if(hayIgual()) transita(Estado.R_MAYI);
+			else return unidadMay();
+		case R_MAYI: return unidadMayi();
 		case R_MEN:
-			if(hayAsignacion()) transita(Estado.R_MENI);
+			if(hayIgual()) transita(Estado.R_MENI);
 			else unidadMen();
 			break;
-		case R_MENI:
-			return unidadId();
+		case R_MENI: return unidadMeni();
 		case R_MENOS:
 			if(hayDigito()) transita(Estado.R_ENT);
 			else unidadMenos();
-			break;
-		case R_NOT:
-			//no se muy bien que hacer aqui
-			break;
-		case R_OR:
-			//no se muy bien que hacer aqui
 			break;
 		case R_P:
 			if(hayDigito()) transita(Estado.R_DEC);
 			else error();
 			break;
-		case R_PAP:
-			return unidadPap();
-		case R_PCI:
-			return unidadPci();
-		case R_POR:
-			return unidadPor();
-		case R_SEC:
-			return unidadId();
+		case R_PAP: return unidadPap();
+		case R_PCI: return unidadPci();
+		case R_POR: return unidadPor();
+		case R_SEC: return unidadSec();
 		case R_VAR:
-			return unidadVar();
+			if(hayLetra() || hayDigito() || hayBarra()) transita(Estado.R_VAR);
+			else return unidadVar();
 		default:
 			error();
 			break;
          }
      }    
    }
+   
    private void transita(Estado sig) throws IOException {
      lex.append((char)sigCar);
      sigCar();         
      estado = sig;
    }
+   
    private void transitaIgnorando(Estado sig) throws IOException {
      sigCar();         
      filaInicio = filaActual;
      columnaInicio = columnaActual;
      estado = sig;
    }
+   
    private void sigCar() throws IOException {
      sigCar = input.read();
      if (sigCar == NL.charAt(0)) saltaFinDeLinea();
@@ -197,6 +180,7 @@ public class AnalizadorLexico {
        columnaActual++;  
      }
    }
+   
    private void saltaFinDeLinea() throws IOException {
       for (int i=1; i < NL.length(); i++) {
           sigCar = input.read();
@@ -212,6 +196,7 @@ public class AnalizadorLexico {
    private boolean hayPunto() {return sigCar == '.';}
    private boolean hayExcl() {return sigCar == '!';}
    private boolean hayAmp() {return sigCar == '&';}
+   private boolean hayBarra() {return sigCar == '_';}
    /*OPERADORES*/
    private boolean haySuma() {return sigCar == '+';}
    private boolean hayResta() {return sigCar == '-';}
@@ -224,13 +209,13 @@ public class AnalizadorLexico {
    private boolean hayMayor() {return sigCar == '>';}
    private boolean hayMenor() {return sigCar == '<';}
    /*ASIGNACIONES*/
-   private boolean hayAsignacion() {return sigCar == '=';}
+   private boolean hayIgual() {return sigCar == '=';}
    /*ESPECIALES*/ 
    private boolean haySep() {return sigCar == ' ' || sigCar == '\t' || sigCar=='\n' || sigCar == '\r' 
 		   							|| sigCar == '\b';}
    private boolean hayEOF() {return sigCar == -1;}
    
-   private UnidadLexica unidadId() {
+   private UnidadLexica unidadVar() {
      switch(lex.toString()) {
          case "true":  
             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.TRUE);
@@ -242,32 +227,33 @@ public class AnalizadorLexico {
              return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.OR); 
          case "not":    
              return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.NOT); 
-         case ">=":    
-             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MAYI);
-         case "<=":    
-             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MENI);
-         case "==":    
-             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.IGUAL);
-         case "!=":    
-             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.DIST);             
-         case "&&":    
-             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.SEC);
-         case "-1":
-        	 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.EOF);
+         case "num":
+        	 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.NUM);
+         case "bool":
+        	 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.BOOL);
          default:    
-            return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.IDEN,lex.toString());     
+        	 return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.VAR,lex.toString());     
       }
    }    
 
    private UnidadLexica unidadMay() {
 	 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MAY);     
    }
-   private UnidadLexica unidadEx() {
-	 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.EX);     
-   }
+   private UnidadLexica unidadMayi() {
+		 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MAYI);     
+	   }
+   private UnidadLexica unidadDist() {
+		 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.DIST);     
+	   }
    private UnidadLexica unidadMen() {
 	 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MEN);     
-   }   
+   }  
+   private UnidadLexica unidadMeni() {
+		 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MENI);     
+	   }
+   private UnidadLexica unidadIgual() {
+		 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.IGUAL);     
+	   }
    private UnidadLexica unidadPap() {
 	 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.PAP);     
    }
@@ -286,18 +272,18 @@ public class AnalizadorLexico {
    private UnidadLexica unidadDiv() {
 	 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.DIV);     
    }	   
-   private UnidadLexica unidadEnt() {
-	 return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.ENT,lex.toString());     
-   }
-   private UnidadLexica unidadNum() {
-	 return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.NUM,lex.toString());     
-   } 
-   private UnidadLexica unidadVar() {
-     return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.VAR,lex.toString());    
-   }    
+   private UnidadLexica unidadNumR() {
+		 return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.NUMR,lex.toString());     
+	   }
    private UnidadLexica unidadAsig() {
      return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.ASIG);     
    }
+   private UnidadLexica unidadSec() {
+		 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.SEC);     
+	   }
+   private UnidadLexica unidadEOF() {
+		 return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.EOF);     
+	   }
     
    private void error() {
      System.err.println("("+filaActual+','+columnaActual+"):Caracter inexperado");  
