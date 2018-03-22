3119fbb0560f6e2c250b41cb8ed42ca128b1270c
abvg9 <velas178@hotmail.com>
2018-03-22 18:33:08 +0100

    Implementación automatica

---
+++ Analizador Sintactico/ImplementacionJavacc/src/asint/Main.java
@@ -0,0 +1,9 @@
+package asint;
+import java.io.FileReader;
+public class Main{
+   public static void main(String[] args) throws Exception {
+      AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(new FileReader(args[0]));
+	  asint.S();
+	  System.out.println("SUCCESS\n");
+   }
+}
