6a9392604dd63819dd637378007a36925508dab9
Elena Kaloyanova Popova <elena.dokrai@gmail.com>
2018-06-23 13:55:13 +0200

    Cambio del main

--- Sintaxis Abstracta/sintaxis_abstracta/src/javaCC/Main.java
+++ Sintaxis Abstracta/sintaxis_abstracta/src/javaCC/Main.java
@@ -1,16 +1,10 @@
 package javaCC;
 
 import java.io.FileReader;
-
-import ast.*;
-
 public class Main{
    public static void main(String[] args) throws Exception {
       Evaluador eval = new Evaluador(new FileReader(args[0]));
-      //System.out.println(eval.Sp());
-      Programa ast = (Programa) eval.Sp();
-      LDs lds = ast.decs();
-      LIs lis = ast.lis();
-      System.out.println(ast);
+      System.out.println(eval.Sp());
+      System.out.println("Análisis terminado" );
    }
 }
