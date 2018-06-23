c41b2abbb953dd9a2dae0cedc029b0a6541d659e
abvg9 <velas178@hotmail.com>
2018-06-23 20:29:29 +0200

    Pequeños arreglos

--- Sintaxis Abstracta/sintaxis_abstracta/src/javaCC/Main.java
+++ Sintaxis Abstracta/sintaxis_abstracta/src/javaCC/Main.java
@@ -2,9 +2,10 @@ package javaCC;
 
 import java.io.FileReader;
 public class Main{
+			
    public static void main(String[] args) throws Exception {
       Evaluador eval = new Evaluador(new FileReader(args[0]));
       System.out.println(eval.Sp());
       System.out.println("Análisis terminado" );
-   }
+   }  
 }
